c2126b4823045fd8915a51c9f0f38ca103a15731
Owen Leonard <owen@balawis.leonard.fam>
2010-09-15 00:23:57 -0600

    scheduling page now only shows one table per individual

--- inc/class.tc.inc.php
+++ inc/class.tc.inc.php
@@ -3570,7 +3570,7 @@ class tc
 		$header_row.= "<th width=$location_width><font size=-2>Location</th>";
 		$table_data = "";
 
-		$sql = "SELECT * FROM tc_presidency where valid=1";
+		$sql = "SELECT * FROM tc_presidency where valid=1 GROUP BY indiv";
 		$this->db->query($sql,__LINE__,__FILE__);
 		$i=0;
 		while ($this->db->next_record()) {
